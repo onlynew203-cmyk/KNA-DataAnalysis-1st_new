bc7010c4c625dc957c2974cdc4d6bc22377b971b
onlynew203-cmyk <onlynew203@gmail.com>
2026-07-15 17:51:30 +0900

    실습연습 연산자

---
+++ 07_ practice.py
@@ -0,0 +1,26 @@
+# 실습 1
+a = 17
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+
+# ----------------------
+
+# 실습 2
+
+x = 2
+y = 6
+z = 10
+
+print((x + y + z)/3)
+print(2 ** 2)
+print(2 * 6 * 10)
+
+# ----------------------
