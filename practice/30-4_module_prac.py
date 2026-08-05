4bf2e453c11164b842096a04fee60bba3f3ebd77
onlynew203-cmyk <onlynew203@gmail.com>
2026-08-05 16:05:14 +0900

    모듈 실습 04번

--- practice/30-4_module_prac.py
+++ practice/30-4_module_prac.py
@@ -4,4 +4,15 @@
 # ③ datetime.now로 현재 시각을 담기
 # ④ f-string으로 파일 수와 시각을 한 문장으로 출력
 
+import os
+import datetime
 
+file_list = os.listdir()
+count_file_list = len(file_list)
+print(count_file_list) # 35
+
+now = datetime.datetime.now() 
+print(now) # 2026-08-05 16:03:05.938497
+
+print(f"파일 수: {count_file_list}, 현재 시각: {now}")
+# 파일 수: 35, 현재 시각: 2026-08-05 16:04:54.133426
