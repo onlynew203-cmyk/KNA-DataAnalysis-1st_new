e29820423c28ecbc8691c612ddbaba6661c393bf
onlynew203-cmyk <onlynew203@gmail.com>
2026-08-11 08:57:41 +0900

    numpy_prac

---
+++ 41_01_08_numpy_prac_02.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# 0부터 30, 6개의 간격
+gab_six = np.arange(0, 30, 6)
+print(gab_six) # [ 0  6 12 18 24]
+
+# 0부터 30까지 6등분
+div_six = np.linspace(0, 30, 6)
+print(div_six) # [ 0.  6. 12. 18. 24. 30.] 실수 형태
+
