b1fdf378539e4876dadfde0f372ca397d00001e2
onlynew203-cmyk <onlynew203@gmail.com>
2026-08-11 08:57:34 +0900

    numpy_zeros

---
+++ 41_01_07_numpy_zeros.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# 0으로 채우기
+block_zeros = np.zeros(5)
+print(block_zeros) # [0. 0. 0. 0. 0.]
+
+# 7로 채우기
+block_seven = np.full(4, 7)
+print(block_seven) # [7 7 7 7]
+
+# 명시적으로 7.0처럼 float값을 지정해줘야
+# float 타입 값들로 채워지는 배열이 만들어진다
+block_seven = np.full(4, 7.0)
+print(block_seven) # [7. 7. 7. 7.]
+
+# ones
+ones_six = np.ones(6)
+print(ones_six) # [1. 1. 1. 1. 1. 1.]
+
