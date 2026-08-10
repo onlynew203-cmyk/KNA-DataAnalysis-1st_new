215e72f85b50877f5c7004e0a75be2b20f4504ce
onlynew203-cmyk <onlynew203@gmail.com>
2026-08-11 08:57:15 +0900

    numpy_arange

---
+++ 41_01_05_numpy_arange.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# 0부터 4까지 생성
+under_five = np.arange(5) # NumPy에서 0부터 5 미만까지의 숫자를 배열로 만드는 코드
+print(under_five) # [0 1 2 3 4] 
+
+# 0부터 8까지 간격 2
+gab_two = np.arange(0, 10, 2) # 0부터 10 전까지, 간격 2로 ((0, 9, 2)도 같은 결과)
+print(gab_two) # [0 2 4 6 8]
+
