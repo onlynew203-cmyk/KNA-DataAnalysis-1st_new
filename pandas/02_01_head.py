fcb37a7d29cdb9a94b5f43dbf653af45ba9b5a6b
onlynew203-cmyk <onlynew203@gmail.com>
2026-08-12 17:27:43 +0900

    실습 추가

--- pandas/02_01_head.py
+++ pandas/02_01_head.py
@@ -44,3 +44,39 @@ df_4 = pd.read_csv("PANDAS/data/12_metro_compressor.csv",
 print(df_4.columns) 
 print(df_4.columns.tolist())
 print(df_4.dtypes) 
+
+print(df_4.info())
+# <class 'pandas.core.frame.DataFrame'>
+# RangeIndex: 200 entries, 0 to 199 ----- # → 200행
+# Data columns (total 7 columns): ------- # → 7열
+#  #   Column  Non-Null Count  Dtype  
+# ---  ------  --------------  -----  
+#  0   측정시각    200 non-null    object 
+#  1   압축압력    200 non-null    float64
+#  2   배출압력    200 non-null    float64
+#  3   저장압력    200 non-null    float64
+#  4   오일온도    199 non-null    float64
+#  5   모터전류    200 non-null    float64
+#  6   가동상태    200 non-null    object 
+# dtypes: float64(5), object(2)
+# memory usage: 11.1+ KB
+# None
+
+
+# 실습 5. info로 데이터 건강검진
+df_5 = pd.read_csv("PANDAS/data/12_metro_digital.csv",
+                 encoding= 'utf-8')
+
+print(df_5.info())
+# <class 'pandas.core.frame.DataFrame'>
+# RangeIndex: 120 entries, 0 to 119
+# Data columns (total 4 columns):
+#  #   Column  Non-Null Count  Dtype 
+# ---  ------  --------------  ----- 
+#  0   측정시각    120 non-null    object
+#  1   압축기     120 non-null    int64 
+#  2   타워      120 non-null    int64 
+#  3   저압스위치   120 non-null    int64 
+# dtypes: int64(3), object(1)
+# memory usage: 3.9+ KB
+# None
